3f9960144f89681fdaba72a236c216660d988f7c
Juan <rigualjuan@gmail.com>
2023-06-12 21:20:13 -0300

    fix: grid styles

--- src/components/card/Card.tsx
+++ src/components/card/Card.tsx
@@ -2,7 +2,7 @@ import { CardProps } from "@src/interfaces/card.interface";
 
 const Card = ({ description, id, imageUrl, title }: CardProps) => {
   return (
-    <div className="card w-96 bg-base-100 shadow-xl">
+    <div className="card w-80 bg-base-100 shadow-xl">
       <figure>
         <img src={imageUrl} alt="Shoes" />
       </figure>
